eb0700582680c61c1b4f09ed4c828c7488e58266
PlamenIvanov <plamen91ivanov@abv.bg>
2022-10-09 08:54:44 +0300

    Add project files.

---
+++ FundamentalsExc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundamentalsExc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
